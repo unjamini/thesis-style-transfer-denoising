01f1c40af12e54f4178d37bb5d62751afb4dde81
Evgeniia Rumiantseva <rumyantseva2000@gmail.com>
2021-02-24 13:07:52 +0300

    Fixed style picker

--- style_picker.py
+++ style_picker.py
@@ -38,17 +38,15 @@ class StylesPicker:
         pass
 
     def compare_func(self, image_mean, image_std, style_idx):
-        return np.sqrt((image_mean - self.style_mean_arr[style_idx]) ** 2) + \
-               np.sqrt((image_std - self.style_std_arr[style_idx]) ** 2)
+        return np.sqrt(np.square(image_mean - self.style_mean_arr[style_idx])) + \
+          np.sqrt(np.square(image_std - self.style_std_arr[style_idx]))
 
     def initialize(self):
         if self.style_lib is None:
             return
         self.style_mean_arr = np.mean(self.style_lib, axis=(1, 2))
         self.style_std_arr = np.std(self.style_lib, axis=(1, 2))
-        print(self.style_lib.shape, self.style_mean_arr.shape)
         self.style_lbp_arr = np.vectorize(get_lbp, signature='(n,m)->(n,n)')(self.style_lib)
-        print(self.style_lbp_arr.shape)
         self.style_hist_arr = np.vectorize(get_hist, signature='(n,m)->(256)')(self.style_lbp_arr)
         print(self.style_lib.shape, self.style_mean_arr.shape, self.style_lbp_arr.shape, self.style_hist_arr.shape)
 
@@ -63,13 +61,12 @@ class StylesPicker:
         image_hist, _ = np.histogram(image_lbp.ravel(), 256, [0, 256])
         func = lambda x: get_corr_with_image(image_hist, x)
         corr_coef_with_styles = np.apply_along_axis(func, 1, self.style_hist_arr)
-        print(corr_coef_with_styles.shape)
-        print(corr_coef_with_styles)
-        top_10_idx = (-corr_coef_with_styles).argsort()[:10]
+        top_10_idx = (-corr_coef_with_styles[..., 0]).argsort()[:10]
         func2 = lambda x: self.compare_func(img_mean, img_std, x)
         compare_values = np.vectorize(func2)(top_10_idx)
         res_idx = top_10_idx[np.argmin(compare_values)]
-        return self.style_lib[res_idx, :, :]
+        print(res_idx)
+        return self.style_lib[res_idx, ...]
 
 
 # s_lib = StylesPicker('/Users/evgenia/Desktop/диплом/cnn-vgg/style_data.hdf5')
